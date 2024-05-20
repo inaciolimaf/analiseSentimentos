7ef2965a432810dc85666f73c4f56db65a11a913
inacio filho <inaciofilho.lima@gmail.com>
2024-05-20 17:24:25 -0300

    Concertando requiriments

--- index.py
+++ index.py
@@ -5,29 +5,20 @@ import tensorflow as tf
 import tensorflow_text as tf_text
 import kagglehub
 
+@st.cache_resource
+def carregar_modelo():
+    path = kagglehub.model_download("inciofilho/testesentimentos1/tensorFlow2/testesentimentos2")
+    custom_objects = {'KerasLayer': tf_hub.KerasLayer}
+    modelo = load_model(f"{path}/meu_modelo.h5", custom_objects=custom_objects)
+    print("Carregado")
+    return modelo
+
 st.title("Análise de sentimento")
-
-class Modelo:
-    _instance = None
-    modelo = None
-    
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-        return cls._instance
-    
-    def __init__(self):
-        path = kagglehub.model_download("inciofilho/testesentimentos1/tensorFlow2/testesentimentos2")
-        custom_objects = {'KerasLayer': tf_hub.KerasLayer}
-        self.modelo = load_model(f"{path}/meu_modelo.h5", custom_objects=custom_objects)
-        print("Carregado")
-
 
 def calcular_sentimento(text):
     print("Iniciou carregamento")
-    modelo = Modelo()
+    pred = carregar_modelo().predict([text])
     print("Terminou")
-    pred = modelo.modelo.predict([text])
     return "Positivo" if pred[0]>=0.5 else "Negativo"
 
 texto_input = st.text_input("Digite o texto aqui:")
